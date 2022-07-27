17de7b815414f891323db4a2c0fe3e6486a5c238
Cappellari, Fabrício (ESI) <fabricio.cappellari@zallpy.com>
2022-07-27 20:44:22 -0300

    adding attack movement for players

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const gravity = 0.7
 
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color}) {
         this.position = position
         this.velocity = velocity
         this.height = 150
+        this.width = 50
         this.lastKey
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50,
+
+        }
+        this.color = color
     }
 
     draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+        // attack box
+        c.fillStyle = 'green'
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
     }
 
     update() {
@@ -41,7 +53,8 @@ const player = new Sprite({
     velocity: {
         x: 0,
         y: 0,
-    }
+    },
+    color: 'red'
 })
 
 const enemy = new Sprite({
@@ -52,7 +65,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0,
-    }
+    },
+    color: 'blue'
 })
 
 const keys = {
@@ -92,6 +106,14 @@ function animate() {
         enemy.velocity.x = -5
     } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5
+    }
+
+    // detect for collision
+    if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x
+    && player.attackBox.position.x <= enemy.position.x + enemy.width
+    && player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+    && player.attackBox.position.y <= enemy.position.y + enemy.height){
+        console.log('attack')
     }
 }
 
